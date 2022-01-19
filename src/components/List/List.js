c1957e72ffe84163716c1e652dc5fcdc2c68d8fd
b00db <48580026+b00db@users.noreply.github.com>
2022-01-19 16:35:34 +0900

    Add InputCard.js, InputContainger.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,6 +3,7 @@ import { Paper, Typography, CssBaseline } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Title from './Title';
 import Card from './Card';
+import InputContainer from '../Input/InputContainer';
 
 const useStyle = makeStyles((theme) => ({
   root: {
@@ -22,6 +23,7 @@ export default function List() {
         <Card />
         <Card />
         <Card />
+        <InputContainer />
       </Paper>
     </div>
   );
