86eca7e05f75bc85d81da709eebdf5119851207e
b00db <48580026+b00db@users.noreply.github.com>
2022-01-19 17:52:37 +0900

    Drag and Drop Card in the different list

--- src/utils/store.js
+++ src/utils/store.js
@@ -20,8 +20,13 @@ const cards = [
         title: 'List 1',
         cards,
       },
+      'list-2': {
+        id: 'list-2',
+        title: 'List 2',
+        cards: [],
+      },
     },
-    listIds: ['list-1'],
+    listIds: ['list-1', 'list-2'],
   };
   
   export default data;
